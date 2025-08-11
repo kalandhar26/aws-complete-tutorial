2c00ff01e2c6f3706049940dd33007981a2aa6a4
babak <babakalandhar499@gmail.com>
2025-08-11 21:00:07 +0530

    Added new Git things1

---
+++ aws-services/src/test/java/com/aws/aws_services/AwsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.aws_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
